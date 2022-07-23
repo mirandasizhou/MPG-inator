a2acef305b159e4af8d020f2d2320092e92223e8
Miranda Zhou <miranda@Mirandas-MacBook-Pro.local>
2022-07-23 12:26:20 -0700

    Configuration to deploy to heroku

--- src/Prices.jsx
+++ src/Prices.jsx
@@ -9,7 +9,7 @@ class Prices extends React.Component {
     if (this.props.state.gasPrice && this.props.state.elecPrice) {
       return (
         <div>
-         <p>The average gas price in {this.props.state.selectedState} is {this.props.state.gasPrice} per gallon</p>
+         <p>The average gas price in {this.props.state.selectedState} is {this.props.state.gasPrice} dollars per gallon</p>
          <p>The average electricity price in {this.props.state.selectedState} is {this.props.state.elecPrice} cents per kW/h</p>
         </div>
       )
